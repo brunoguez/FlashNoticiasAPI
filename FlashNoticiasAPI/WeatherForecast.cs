2113120b57bbada1c3fd902ece5a46fdbd8fdbf1
brunoguez <brunomguez@gmail.com>
2023-05-16 19:13:01 -0300

    Initial commit

---
+++ FlashNoticiasAPI/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace FlashNoticiasAPI
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
